e9c728db9c0d0177bce32f99034f9135aeb507a4
Ubuntu <ubuntu@ip-172-31-50-37.ec2.internal>
2021-02-05 02:34:30 +0000

    adde

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request
+import pandas as pd
+import joblib
+import json
 
 app = Flask(__name__)
 
@@ -7,8 +10,26 @@ def upload():
    return render_template('index.html')
 
 def make_prediction(file):
+    dummpy_df = pd.read_pickle("dummy.pkl")
+    clf = joblib.load('model.pkl')
     
-    return 0.09
+    # Opening JSON file 
+    f = open(file) 
+
+    # returns JSON object as  
+    # a dictionary 
+    data = json.load(f)
+
+    data_final = {"Age":[int(data['Age'])],
+              "Embarked": [data["Embarked"]],
+              "Sex":[data["Sex"]]
+              
+              }
+    q_df = pd.DataFrame(data_final)
+    query = pd.get_dummies(q_df)
+    final_q = query.combine_first(dummpy_df)
+    prediction = clf.predict(final_q)
+    return prediction[0]
 
 
 @app.route('/uploader', methods = ['GET', 'POST'])
@@ -18,9 +39,11 @@ def uploader():
     if request.method == 'POST':
         f = request.files['file']
         f.save(f.filename)
-        prediction = make_prediction(f.filename)
-        return render_template('index.html',prediction = "The Prediction is {}".format(prediction))
-    
+        try:
+            prediction = make_prediction(f.filename)
+            return render_template('index.html',prediction = "The Prediction is {}".format(prediction))
+        except:
+            render_template('index.html',prediction = "Something Went Wrong Mate!!!!!!)
     
     
 if __name__ == '__main__':
